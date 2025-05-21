b1a77af275daaf40865a4d11cf312879c7928439
ShanDSkeptiC <shandyka79@gmail.com>
2025-05-21 18:51:56 +0700

    Add files via upload

---
+++ src/audio.d.ts
@@ -0,0 +1,14 @@
+declare module '*.wav' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.mp3' {
+	const content: string;
+	export default content;
+}
+
+declare module '*.ogg' {
+	const content: string;
+	export default content;
+}
